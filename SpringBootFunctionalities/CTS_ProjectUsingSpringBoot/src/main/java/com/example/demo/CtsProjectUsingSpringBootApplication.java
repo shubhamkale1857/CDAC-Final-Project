3d377b7433f11565db708f8dcc1402a2e871a5e2
DiwateManasi <154223721+DiwateManasi@users.noreply.github.com>
2024-02-06 15:24:31 +0530

    change in comment

--- SpringBootFunctionalities/CTS_ProjectUsingSpringBoot/src/main/java/com/example/demo/CtsProjectUsingSpringBootApplication.java
+++ SpringBootFunctionalities/CTS_ProjectUsingSpringBoot/src/main/java/com/example/demo/CtsProjectUsingSpringBootApplication.java
@@ -9,7 +9,6 @@ public class CtsProjectUsingSpringBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CtsProjectUsingSpringBootApplication.class, args);
 	}
-	//this comment im adding for checking
 	//this is comment from shubz
 
 }
