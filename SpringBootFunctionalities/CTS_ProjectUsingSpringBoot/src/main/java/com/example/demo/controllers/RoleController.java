4b671da05308d08f04824c5da9d95708e4cdba42
DiwateManasi <154223721+DiwateManasi@users.noreply.github.com>
2024-02-08 10:36:02 +0530

    cors added

--- SpringBootFunctionalities/CTS_ProjectUsingSpringBoot/src/main/java/com/example/demo/controllers/RoleController.java
+++ SpringBootFunctionalities/CTS_ProjectUsingSpringBoot/src/main/java/com/example/demo/controllers/RoleController.java
@@ -1,11 +1,13 @@
 package com.example.demo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.services.RoleService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class RoleController {
 	@Autowired
 	RoleService service;
--- SpringBootFunctionalities/CTS_ProjectUsingSpringBoot/src/main/java/com/example/demo/controllers/UserController.java
+++ SpringBootFunctionalities/CTS_ProjectUsingSpringBoot/src/main/java/com/example/demo/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import com.example.demo.entiies.User;
 import com.example.demo.services.UserService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 	@Autowired
 	UserService uservice;
