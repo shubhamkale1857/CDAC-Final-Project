15fd9038c58d91b60f707ae2fa4bc9ec3c065042
DiwateManasi <154223721+DiwateManasi@users.noreply.github.com>
2024-02-21 20:49:03 +0530

    Update MealHistoryComp.js

--- UI/projectui/src/components/MealHistoryComp.js
+++ UI/projectui/src/components/MealHistoryComp.js
@@ -12,42 +12,49 @@ export const MealHistory = ()=>{
     const[luflag, setLuflag]= useState(false);
     const[snflag, setSnflag]= useState(false);
     const[diflag, setDiflag]= useState(false);
+    const[flag, setFlag]= useState(true);
     const navigate = useNavigate();
 
     useEffect(()=>{
         fetch("http://localhost:8080/getMealHistorytoday?custid="+user.customer_id)
         .then(resp => resp.json())
         .then(data => { 
-            setObj(data)
-            var arr = obj.filter((o)=>{
+            if(data.length!==0){
+                setFlag(true)
+            console.log(data)
+            var arr = data.filter((o)=>{
                 return o[4]===1
             })
             if(arr.length!==0)
             {
                 setBrflag(true);
-            }
-            arr = obj.filter((o)=>{
+            }else{setBrflag(false)}
+            arr = data.filter((o)=>{
                 return o[4]===2
             })
             if(arr.length!==0)
             {
                 setLuflag(true);
-            }
-            arr = obj.filter((o)=>{
+            }else{setLuflag(false);}
+            arr = data.filter((o)=>{
                 return o[4]===3
             })
             if(arr.length!==0)
             {
                 setSnflag(true);
-            }
-            arr = obj.filter((o)=>{
+            }else{setSnflag(false);}
+            arr = data.filter((o)=>{
                 return o[4]===4
             })
             if(arr.length!==0)
             {
                 setDiflag(true);
+            }else{setDiflag(false);}
+            setObj(data)}
+            else{
+                setFlag(false)
             }
-            return ;
+            
         })
         .catch(() => navigate("/ErrorPage"))
     },[])
@@ -56,36 +63,41 @@ export const MealHistory = ()=>{
         fetch("http://localhost:8080/getMealHistory?custid="+user.customer_id+"&date="+date2)
         .then(resp => resp.json())
         .then(data => {
-            setObj(data)
-            var arr = obj.filter((o)=>{
+            if(data.length!==0){
+                setFlag(true)
+            var arr = data.filter((o)=>{
                 return o[4]===1
             })
             if(arr.length!==0)
             {
                 setBrflag(true);
-            }
-            arr = obj.filter((o)=>{
+            }else{setBrflag(false)}
+            arr = data.filter((o)=>{
                 return o[4]===2
             })
             if(arr.length!==0)
             {
                 setLuflag(true);
-            }
-            arr = obj.filter((o)=>{
+            }else{setLuflag(false);}
+            arr = data.filter((o)=>{
                 return o[4]===3
             })
             if(arr.length!==0)
             {
                 setSnflag(true);
-            }
-            arr = obj.filter((o)=>{
+            }else{setSnflag(false);}
+            arr = data.filter((o)=>{
                 return o[4]===4
             })
             if(arr.length!==0)
             {
                 setDiflag(true);
+            }else{setDiflag(false);}
+            setObj(data)}
+            else{
+                setFlag(false)
             }
-            return ;
+            
         })
 
         const currentDate = new Date();
@@ -110,11 +122,10 @@ export const MealHistory = ()=>{
             </form>
 
             <br/><br/><br/>
-            
-
+            <div style={{display: flag?"block":"none"}}>
             <h3  style={{color:"rgb(35, 110, 202)"}}>{date}'s Overview</h3><br/>
 
-            <div style={{display:brflag?"block":"none"}} >
+            <div style={{display: brflag?"block":"none"}} >
             <h5>Breakfast</h5>
             <table className="table table-bordered">
                 <thead className="table-info">
@@ -142,7 +153,7 @@ export const MealHistory = ()=>{
             </table>
             </div>
 
-            <div  style={{display:luflag?"block":"none"}}>
+            <div  style={{display: luflag?"block":"none"}}>
             <h5>Lunch</h5>
             <table className="table table-bordered">
                 <thead className="table-info">
@@ -169,7 +180,7 @@ export const MealHistory = ()=>{
             </table>
             </div>
 
-            <div style={{display:snflag?"block":"none"}}>
+            <div style={{display: snflag?"block":"none"}}>
             <h5>Snack</h5>
             <table className="table table-bordered">
                 <thead className="table-info">
@@ -196,7 +207,7 @@ export const MealHistory = ()=>{
             </table>
             </div>
 
-            <div style={{display:diflag?"block":"none"}}>
+            <div style={{display: diflag?"block":"none"}}>
             <h5>Dinner</h5>
             <table className="table table-bordered">
                 <thead className="table-info">
@@ -222,6 +233,8 @@ export const MealHistory = ()=>{
                 </tbody>
             </table>
             </div>
+        </div>
+        <h3 style={{display: flag?"none":"block" , color:"rgb(35, 110, 202)"}}>No History for this day!!</h3>
         </div>
     )
 }
